c890a0abc9f517df4a95d1da8da234df27ba2b51
agungrio307 <agungrio307@gmail.com>
2025-10-08 11:12:04 +0700

    Percobaan 2

---
+++ Jobsheet5/switchCetakKRS02.java
@@ -0,0 +1,45 @@
+package Jobsheet5;
+import java.util.Scanner;
+
+public class switchCetakKRS02 {
+    public static void main(String[] args) {
+        
+        int semester02;
+
+        Scanner agung = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Masukkan Semester saat ini: ");
+        semester02 = agung.nextInt();
+
+        switch (semester02) {
+            case 1:
+                System.out.println("KRS Semester 1 ditampilkan");
+                break;
+            case 2:
+                System.out.println("KRS Semester 2 ditampilkan");
+                break;
+            case 3:
+                System.out.println("KRS Semester 3 ditampilkan");
+                break;
+            case 4:
+                System.out.println("KRS Semester 4 ditampilkan");
+                break;
+            case 5:
+                System.out.println("KRS Semester 5 ditampilkan");
+                break;
+            case 6:
+                System.out.println("KRS Semester 6 ditampilkan");
+                break;
+            case 7:
+                System.out.println("KRS Semester 7 ditampilkan");
+                break;
+            case 8:
+                System.out.println("KRS Semester 8 ditampilkan");
+                break;
+            default:
+                System.out.println("Semester tidak valid");
+
+        }
+    }
+}
