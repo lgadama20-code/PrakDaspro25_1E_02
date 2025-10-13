e6be90a00f67ce71fba7ff25b74af41ca6ed2ce1
Agunggg <agungrio307@gmail.com>
2025-10-14 01:12:08 +0700

    finally

--- P6/CM_SIAKAD_02.java
+++ P6/CM_SIAKAD_02.java
@@ -6,7 +6,7 @@ public class CM_SIAKAD_02 {
         
         int pancasila02, daspro02;
         int nilai_pancasila02, nilai_daspro02;
-        double setara_pancasila02= 0, setara_daspro02 = 0, setara_bing02 = 0;
+        double setara_pancasila02= 0.0, setara_daspro02 = 0.0, setara_bing02 = 0.0;
         double ip02;
         double bobot02;
         String grade02 = "", grade_daspro02 = "";
@@ -29,89 +29,76 @@ public class CM_SIAKAD_02 {
         System.out.println("=================================================== HASIL KONVERSI NILAI MAHASISWA ==================================================");
         System.out.println("Nama : Agung Satrio Gusti");
         System.out.println("NIM  : 254107020228");
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------");
         System.out.print("Mata Kuliah");
         System.out.print("\t\tSKS");
         System.out.print("\tNilai");
         System.out.print("\t\thuruf");
         System.out.println("\t\t  Setara");
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------");
         System.out.print("Pancasila");
-        System.out.print("\t\t2");
-        System.out.print("        " + pancasila02);
-        System.out.print("\t" + grade02);
-        System.out.print("\t\t\t  " + setara_pancasila02);
+        System.out.print("\t\t 2");
+        System.out.print("       " + pancasila02);
 
-         if (pancasila02 > 80 ) {
+        if (daspro02 < 0 || daspro02 > 100 || pancasila02 < 0 || pancasila02 > 100) {
+            System.out.println("\t[ERROR] Nilai tidak valid! Semua nilai harus berada di antara 0 - 100.");
+        } else if (pancasila02 > 80 ) {
             grade02 = "A";
             setara_pancasila02 = 4;
-            System.out.println(setara_pancasila02);
         } else if (pancasila02 > 73) {
             grade02 = "B+";
             setara_pancasila02 = 3.5;
-            System.out.println(setara_pancasila02);
         } else if (pancasila02 > 65) {
             grade02 = "B";
             setara_pancasila02 = 3;
-            System.out.println(setara_pancasila02);
         } else if (pancasila02 > 60) {
             grade02 = "C+";
             setara_pancasila02 = 2.5;
-            System.out.println(setara_pancasila02);
         } else if (pancasila02 > 50) {
             grade02 = "C";
             setara_pancasila02 = 2;
-            System.out.println(setara_pancasila02);
         } else if (pancasila02> 39) {
             grade02 = "D";
             setara_pancasila02 = 1;
-            System.out.println(setara_pancasila02);
-        } else if (pancasila02 > 0){
-            grade02 = "E";
-            //S;
-            setara_pancasila02 = 0;
-            System.out.println(setara_pancasila02);
         } else {
-            System.out.println("[ERROR] Nilai Tidak valid! Semua nilai harus berada di antara 0 - 100");
+            grade02 = "E";
+            setara_pancasila02 = 0;
         }
 
-        System.out.print("Dasar Pemograman");
-        System.out.print("\t3");
-        System.out.print("        " + daspro02);
-        System.out.print("\t\t\t" + grade_daspro02);
-        System.out.print("\t  " + setara_daspro02);
+        System.out.print("\t\t  " + grade02);
+        System.out.println("\t\t   " + setara_pancasila02);
 
-        if (daspro02 > 80 ) {
+        System.out.print("Dasar Pemograman");
+        System.out.print("\t 3");
+        System.out.print("       " + daspro02);
+
+        if (daspro02 < 0 || daspro02 > 100 || pancasila02 < 0 || pancasila02 > 100) {
+            System.out.println("\t[ERROR] Nilai tidak valid! Semua nilai harus berada di antara 0 - 100.");
+        } else if (daspro02 > 80 ) {
             grade_daspro02 = "A";
             setara_daspro02 = 4;
-            System.out.println(setara_daspro02);
         } else if (daspro02 > 73) {
             grade_daspro02 = "B+";
             setara_daspro02 = 3.5;
-            System.out.println(setara_daspro02);
         } else if (daspro02 > 65) {
             grade_daspro02 = "B";
             setara_daspro02 = 3;
-            System.out.println(setara_daspro02);
         } else if (daspro02 > 60) {
             grade_daspro02 = "C+";
             setara_daspro02 = 2.5;
-            System.out.println(setara_daspro02);
         } else if (daspro02 > 50) {
            grade_daspro02 = "C";
             setara_daspro02 = 2;
-            System.out.println(setara_daspro02);
         } else if (daspro02> 39) {
             grade_daspro02 = "D";
             setara_daspro02 = 1;
-            System.out.println(setara_daspro02);
-        } else if (daspro02> 0) {
+        } else {
             grade_daspro02 = "E";
             setara_daspro02 = 0;
-            System.out.println(setara_daspro02);
-        } else {
-            System.out.println("[ERROR] Nilai Tidak valid! Semua nilai harus berada di antara 0 - 100");
         }
+
+        System.out.print("\t\t  " + grade_daspro02);
+        System.out.println("\t\t   " + setara_daspro02);
 
         // System.out.print("Bahasa Inggris 1");
         // System.out.print("\t2");
@@ -168,27 +155,19 @@ public class CM_SIAKAD_02 {
         bobot02 = (setara_pancasila02*2) + (setara_daspro02*3);
         ip02 = bobot02 / (7);
 
-        System.out.println("-".repeat(90));
+        System.out.println("-".repeat(133));
         System.out.println("Total SKS  : " + 7);
         System.out.println("Total Bobot: " + bobot02);
         System.out.println("Indeks Prestasi (IP) : " + ip02);
 
-        if (ip02 >= 2.00) {
-            System.out.println("lulus");
+        if (pancasila02 <= 39 || daspro02 <= 39) {
+            System.out.println("Tidak lolos karena terdapat nilai E");
+        } else if (pancasila02 <= 60 || daspro02 <= 60) {
+            System.out.println("Tidak lolos karena terdapat nilai C/D");
+        } else if (ip02 < 2.00) {
+            System.out.println("Tidak lolos karena IP kurang dari 2.00");
         } else {
-            System.out.println("Tidak Lulus");
-        }
-        
-        if (pancasila02 > 39 && daspro02 > 39){
-            System.out.println("Lulus");
-        } else {
-            System.out.println("Tidak LULUS (Nilai C atau D)");
-        } 
-        
-        if (pancasila02 < 1 && daspro02 < 1) {
-            System.out.println("Lulus");
-        } else {
-            System.out.println("Terdapat Nilai E");
+            System.out.println("Lolos");
         }
        
 
