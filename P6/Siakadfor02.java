e35400d15c397b3951f41f7288d5f2efca17bcd6
Agunggg <agungrio307@gmail.com>
2025-10-28 10:56:18 +0700

    Percobaan 1  Modifikasi

--- P6/Siakadfor02.java
+++ P6/Siakadfor02.java
@@ -5,7 +5,7 @@ public class Siakadfor02 {
     public static void main(String[] args) {
         
         double nilai02, terendah02 = 100, tertinggi02 = 0;
-
+        int clulus02 = 0, cTidakLulus02 = 0;
         Scanner agung = new Scanner(System.in);
 
         for (int i = 1; i <= 10; i++) {
@@ -18,9 +18,17 @@ public class Siakadfor02 {
             if (nilai02 < terendah02) {
                 terendah02 = nilai02;
             }
+
+            if (nilai02 > 60) {
+                clulus02++;
+            } else {
+                cTidakLulus02++;
+            }
         }
 
         System.out.println("Nilai Tertinggi : " + tertinggi02);
         System.out.println("Nilai Terendah : " + terendah02);
+        System.out.println("Lulus : " + clulus02);
+        System.out.println("Tidak Lulus : " + cTidakLulus02);
     }
 }
