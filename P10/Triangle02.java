1d5c8bb18b84002cf33f41f69da2dc41b79097ef
Adamavito12 <email.lgadam20@gmail.com>
2025-10-30 12:20:38 +0700

    Perboaan 3 (Finish)

--- P10/Triangle02.java
+++ P10/Triangle02.java
@@ -3,7 +3,27 @@ import java.util.Scanner;
 public class Triangle02 {
     public static void main(String[] args) {
         
-        int n;
+        int n, i = 0;
+        
+        Scanner agung = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n = ");
+        n = agung.nextInt();
+
+        while (i <= n) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+
+            System.out.println("*");
+
+            i++;
+
+        }
+
+
         
     }
 }
